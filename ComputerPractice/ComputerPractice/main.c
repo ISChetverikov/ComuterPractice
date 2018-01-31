ed5836e0f95cbb9fc6cf98797e5f16a8dbf3690e
Четвериков Илья <ischetverikov@gmail.com>
2018-01-31 17:01:20 +0300

    Changed ams inline code

--- ComputerPractice/ComputerPractice/main.c
+++ ComputerPractice/ComputerPractice/main.c
@@ -1,35 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
 
-int func_asm(char x, short int y, char z) {
+short int func_asm(char x, short int y, char z) {
 	short int result = 0;
 	_asm {
-		xor eax, eax
-		xor ecx, ecx
-		xor edx, edx
+
+		mov al, x		// x в al
+		cbw
+		mov bx, 3d
+		imul bx			// умножаем ax на 3
+		dec ax			// уменьшаем ax на 1
+
+		push ax			// сохраняем вычисленный знаменатель дроби в стэке
 
 		mov al, z       // z в al
 		cbw				// расширение до слова
 		add ax, 7d		// в ax находится z + 7
 
-		imul ax, 7d     // умножаем ax на 7
+		mov bx, 7d
+		imul bx			// умножаем ax на 7
 
-		mov dx, y		// у в dx
-		sub ax, dx		// вычитаем из значения в ax значение y
+		mov bx, y		
+		sub ax, bx		// вычитаем y
 
-		push ax			// сохраняем вычисленный числитель дроби в стэке
-
-		mov al, x		// x в al
-		cbw				// расширение до слова
-		imul ax, 3d		// умножаем ax на 3
-		dec ax			// уменьшаем ax на 1
-
-		pop cx			// достаем числитель из стэка
-
-		xchg cx, ax		// обмен числителя и знаменателя
-
-		cwd
-		idiv cx			// деление числителя на знаменатель
+		pop bx			// достаем знаменатель из стэка
+		
+		idiv bx			// деление числителя на знаменатель
 
 		add ax, 2d		// результат увеличиваем на 2
 
@@ -57,12 +54,11 @@ int main() {
 
 	printf_s("Второй тестовый набор:\n");
 	value_asm = func_asm(-0x6b, -0x3cf4, 0x74);
-	value_c = func_asm(-0x6b, -0x3cf4, 0x74);
+	value_c = func(-0x6b, -0x3cf4, 0x74);
 	printf("\tРезультат С: %X (16) или %d (10)\n", value_c, value_c);
 	printf("\tРезультат Assembler: %X (16) или %d (10)\n", value_asm, value_asm);
 	
 	printf_s("========================\n");
-
 
 	return 0;
 }
